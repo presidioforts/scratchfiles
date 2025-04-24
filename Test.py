f5979d60747304a3cf695661fbc7af7caa8d32ce
presidioforts <presidio2025@gmail.com>
2025-04-23 17:23:35 -0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,3 +7,6 @@ pip download \
   --abi cp312 \
   --no-deps \
   tokenizers==0.13.3
+
+
+wget https://files.pythonhosted.org/packages/27/fe/4916ec8f2fc6a7a6dfaa2dd21622e969e813429deff546a09b17b121dee9/tokenizers-0.13.3-cp312-cp312-win_amd64.whl
